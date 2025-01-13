00f0a602d846cf50c5bae3325ffe7f146b0d9cd1
Roekpasawat Sajjapan <75149104+Lockps@users.noreply.github.com>
2025-01-13 19:48:04 +0700

    first commit

---
+++ Yam/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-400 flex">
+      <div className="min-w-[15%] min-h-screen bg-black">asd</div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Yam/src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../index.css";
+
+const Navbar = () => {
+  return <div>lock</div>;
+};
+
+export default Navbar;
