5055ddcff71b111f8f9ad890f61bedc61a2b9e49
Oakkk <oak.goldcup@gmail.com>
2025-04-12 15:25:45 +0700

    yeettttt

--- Yam/src/App.jsx
+++ Yam/src/App.jsx
@@ -12,20 +12,28 @@ const App = () => {
   };
 
   return (
-    <div className="min-h-screen bg-mainbg flex relative">
-      <AnimationBg />
-      <div
-        className={`m-[0.5%] bg-tran transition-all duration-300 ease-in-out ${
-          openNav ? "w-[15%]" : "w-0"
-        } overflow-hidden z-20`}
-      >
-        <Navbar />
-      </div>
-      <div
-        className="absolute top-[3%] left-[1.5%]  bg-transparent z-20"
-        onClick={handleNavbar}
-      >
+    <div className="min-h-screen bg-mainbg flex">
+      {/* ครึ่งซ้ายที่มีรูปภาพ */}
+      <div className="left-side">
         
+      </div>
+
+      {/* ครึ่งขวาที่มี Navbar */}
+      <div className="flex-1 relative">
+        <AnimationBg />
+        <div
+          className={`m-[0.5%] bg-tran transition-all duration-300 ease-in-out ${
+            openNav ? "w-[15%]" : "w-0"
+          } overflow-hidden z-20`}
+        >
+          <Navbar />
+        </div>
+        <div
+          className="absolute top-[3%] left-[1.5%] bg-transparent z-20"
+          onClick={handleNavbar}
+        >
+          
+        </div>
       </div>
     </div>
   );
