5d5a9f26b60c7688a381f64b07057211919dbb27
Roekpasawat Sajjapan <75149104+Lockps@users.noreply.github.com>
2025-04-30 22:59:41 +0700

    Page Layout2

--- Yam/src/App.jsx
+++ Yam/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 import ProfilePage from "./Page/ProfilePage";
+import DashboardPage from "./Page/DashboardPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   );
 }
